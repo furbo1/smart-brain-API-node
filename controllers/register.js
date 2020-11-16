20820b804a6986d86136e82d36648ade74718643
Alex Cocan <al_razvan@yahoo.com>
2020-11-16 12:21:14 +0100

    err message added

--- controllers/register.js
+++ controllers/register.js
@@ -28,7 +28,9 @@ const handleRegister = (req, res, db, bcrypt) => {
             .then(trx.commit)
             .catch(trx.rollback)
     })
-        .catch(err => res.status(400).json('unable to register'));
+        .catch(err => {
+            res.status(400).json({message: err})
+        })
 }
 
 
